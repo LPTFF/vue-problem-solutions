cc3008be524fc61dbff256e78267a5fb6f352f02
lptff <1501683500@qq.com>
2022-07-30 17:31:46 +0800

    完成算法

--- src/views/leetcode/number9.js
+++ src/views/leetcode/number9.js
@@ -3,21 +3,30 @@
 回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
 例如，121 是回文，而 123 不是。 
 */
-const addLow = function (list, target) {
-    //转字符串
-    let listString = String(list);
-    for (let index = 0; index < list.length; index++) {
-        for (let indexOne = index + 1; indexOne < list.length; indexOne++) {
-            if (list[index] + list[indexOne] == target) {
-                return [index, indexOne];
+const addLow = function (list) {
+    let listString = String(list).split('');//先转字符串再转数组
+    console.log(listString);
+    console.log(parseInt((listString.length) / 2));
+    let stopLength = parseInt((listString.length) / 2);
+    for (let index = 0; index < stopLength; index++) {
+        console.log(index);
+        if (listString[index] == listString[listString.length - index - 1]) {
+            console.log(index);
+            if (index == stopLength - 1) {
+                return true;
+            } else {
+                continue;
             }
+        } else {
+            console.log(index);
+            return false;
         }
     }
-    return [' ', ''];
+    return false;
 };
 
-const num = [3, 3], target = 6;
+const num = 1234554321;
 
-const result = addLow(num, target);
+const result = addLow(num);
 
 result;
